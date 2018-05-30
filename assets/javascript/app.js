95c86d76f60b8c1b2d51b9ec0bf50fb8ab7dfeb8
jsbryce1 <jedi1991@hotmail.com>
2018-05-30 00:56:31 -0600

    words

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,118 +1,92 @@
-$(document).ready(function(){
-      // Initial array of images
-      var images = ["cats","dogs","pizza","hugs",];
+$(document).ready(function () {
 
-      // displayImages function re-renders the HTML to display the appropriate content
-      function displayImages() {
+    var images = ["cats", "dogs", "pizza", "hugs", ];
 
+    function displayImages() {
         var image = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + image + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-        // Creates AJAX call for the button being clicked
+        // sends out an ajax request
         $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response){
-          $("#dump-gifs").empty();
+            url: queryURL,
+            method: "GET"
+            // "Promise" after the data gets back from the API request
+        }).done(function (response) {
+            $("#dump-gifs").empty();
+            var results = response.data;
 
-          var results = response.data;
+            // runs through the results and limits it to 10
+            for (var i = 0; i < results.length; i++) {
+                var imageDiv = $("<div>");
+                //stores the ratings in the imageDiv varianle after being run through the for loop
+                var rating = results[i].rating;
+                // Make a h2 element with jQuery and add the rating text inside and store it in the variable of p
+                var p = $("<h2>").text("Rating: " + rating);
 
-          // Retrieves the Rating Data
-          console.log(response);
+                var randomImage = $("<img>");
+                randomImage.attr("src", results[i].images.fixed_height_still.url);
+                randomImage.attr("data-still", results[i].images.fixed_height_still.url);
+                randomImage.attr("data-animate", results[i].images.fixed_height.url);
+                randomImage.attr("data-state", "still");
+                randomImage.addClass('randoImage');
 
-          // Loops the image for limit 10
-          for(var i = 0; i < results.length; i++) {
+                // Append the p variable to the imageDiv variable.
+                imageDiv.prepend(p);
 
-            // Creates a div to hold the image
-            var imageDiv = $("<div>");
-
-            // Make the class for style.css
-            imageDiv.addClass("randomPictures");
-
-            // Creates an element to have the rating displayed
-            var rating = results[i].rating;
-            var p = $("<h2>").text("Rating: " + rating);
-
-            // The Images can still or animate to call the class "randomImage" for click.
-            var randomImage = $("<img>");
-            randomImage.attr("src", results[i].images.fixed_height_still.url);
-            randomImage.attr("data-still", results[i].images.fixed_height_still.url);
-            randomImage.attr("data-animate", results[i].images.fixed_height.url);
-            randomImage.attr("data-state", "still");
-            randomImage.addClass('randoImage');
-
-            // Displays the rating
-            imageDiv.prepend(p);
-
-            // Displays the image
-            imageDiv.prepend(randomImage);
-            $("#dump-gifs").prepend(imageDiv);
-          }
-
-          //if the variable state is equal to 'still',
-          // then update the src attribute of this image to it's data-animate value,
-          // and update the data-state attribute to 'animate'.
-          // If state does not equal 'still', then update the src attribute of this
-          // image to it's data-animate value and update the data-state attribute to 'still'
-          $(".randoImage").on("click", function() {
-            var state = $(this).attr("data-state");
-            console.log(state);
-
-            if (state === "still") {
-              $(this).attr("src", $(this).attr("data-animate"));
-              $(this).attr("data-state", "animate");
-            } else {
-              $(this).attr("src", $(this).attr("data-still"));
-              $(this).attr("data-state", "still");
+                // Append the randomImage variable to the imageDiv variable.
+                imageDiv.prepend(randomImage);
+                // Prepend the imageDiv variable to the element with an id of dump-gifs.
+                $("#dump-gifs").prepend(imageDiv);
             }
-          });
-        });        
-      }
 
-      // Function for displaying image data
-      function renderButtons() {
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+            $(".randoImage").on("click", function () {
+                var state = $(this).attr("data-state");
 
-        // Deletes the gifs prior to adding new gifs
-        // (this is necessary otherwise you will have repeat buttons)
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+            });
+        });
+    }
+ 
+    function renderButtons() {
+       // empties the new-button div before adding new ones
         $("#new-button").empty();
-
-        for(var i = 0; i < images.length; i++) {
-
-          // Then dynamicaly generates buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var imageAdd = $("<button>");
-
-          // Adds a class of image to our button
-          imageAdd.addClass("image");
-
-          // Added a data-attribute
-          imageAdd.attr("data-name", images[i]);
-
-          // Provided the initial button text
-          imageAdd.text(images[i]);
-
-          // Added the button to the image-buttons div
-          $("#new-button").append(imageAdd);
+        //loops through the array of gifs
+        for (var i = 0; i < images.length; i++) {
+            // creates button for each movie in the array
+            var imageAdd = $("<button>");
+            // Adds a class of image to our button
+            imageAdd.addClass("image");
+            // Added a data-attribute with the value of images at index [i]
+            imageAdd.attr("data-name", images[i]);
+            // Provided the initial button text
+            imageAdd.text(images[i]);
+            // adds the button to the html
+            $("#new-button").append(imageAdd);
         }
-      }
+    }
 
-      // This function handles events where the add image button is clicked
-      $("#add-gif").on("click", function(event){
+    // when the submit input button is clicked, run this function
+    $("#add-gif").on("click", function (event) {
         event.preventDefault();
-
-        // This line of code will grab the input from the textbox
+        // grabs the user input from the text box
         var image = $("#gif-input").val().trim();
-
-        // The image from the textbox is then added to our array
+        // pushes the new gif to the images array
         images.push(image);
-
         // Calling renderButtons which handles the processing of our gif array
         renderButtons();
-      });
+    });
 
-      // Adding click event listeners to all elements with a class of "image"
-      $(document).on("click", ".image", displayImages);
-
-      // Calling the renderButtons function to display the intial buttons
-      renderButtons();
+    // Adding click event listeners to all elements with a class of "image"
+    $(document).on("click", ".image", displayImages);
+    // displays the initial list of gifs
+    renderButtons();
 });
